75ed6901083e64d159ae363f843b23450beb9de3
Dwi Pasca <dwipascanursanti020@gmail.com>
2024-03-23 13:43:46 +0700

    Time: 60 ms (35.11%), Space: 49.4 MB (95.21%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,29 @@
+// we can use two pointer to fix this issue
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        let currentSum = numbers[left] + numbers[right]
+
+        // if the current sum value is morethe target then
+        // we need to move the pointer right to the left 1 step
+        if (currentSum > target) {
+            right--
+        } 
+        // if currentSum is lower than target then we need to move pointer left
+        // to the right 1 step
+        else if (currentSum < target) {
+            left++
+        } else {
+            console.log([left, right])
+            return [left + 1, right+1]
+        }
+    }
+};
