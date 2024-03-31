04eae1aab5eaabd0fe8ccc74845cbc07487ac548
Dwi Pasca <dwipascanursanti020@gmail.com>
2024-03-31 20:51:05 +0700

    Time: 107 ms (82.29%), Space: 61.9 MB (88.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+    const map = new Map()
+    for (const str of strs) {
+        let key = str.split('').sort().join('')
+
+        if (map.has(key)) {
+            map.set(key, [...map.get(key), str])
+        } else {
+            map.set(key, [str])
+        }
+        
+    }
+
+    return Array.from(map.values())
+
+};
