635b60244bc35194b99d6929eccda386581ab5af
Dwi Pasca <dwipascanursanti020@gmail.com>
2024-08-05 19:58:00 +0800

    Time: 70 ms (29.91%), Space: 51.4 MB (80.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,19 +1,23 @@
+// can use two pointer to fix this issue
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    // based on description the return it's all alphanumeric with space and all of the char is lower case
     const cleanText = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
+
     let left = 0;
-    let right = cleanText.length - 1;
+    let right = cleanText.length -1
 
     while (left < right) {
         if (cleanText[left] !== cleanText[right]) return false
-
         left++;
         right--;
     }
-
     return true
-    
 };
+
+// big O time = O(N)
+// big O space = O(N)
